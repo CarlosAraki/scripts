eed278950195f5353f04ee1ea33f4d90ebe0c632
CarlosAraki <carlosarak@gmail.com>
2018-05-27 22:53:48 -0300

    bug fixes

---
+++ bandeco/bandeco.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#script rapidinho serve pra ver o q tem pra comer como prato principal no bandeco da Unicamps =)
+
+
+wget --tries=2 --connect-timeout=5 -q https://www.prefeitura.unicamp.br/apps/site/cardapio.php -O cardapio
+#-s se arquivo existe e nao é nulo
+if [ -s cardapio ]; 
+	then
+	cat cardapio | sed -i '/PRINCIPAL/!d' cardapio 
+	python remove_all.py
+	rm cardapio
+
+
+else
+	echo "Sem net"
+	rm ./--tries=1
+fi
+
+exit
---
+++ bandeco/remove_all.py
@@ -0,0 +1,9 @@
+ #!/usr/bin/env python
+
+with open('cardapio', 'r') as noisnafita:
+    data=noisnafita.read()
+data1 = data
+data1 = data1.replace('							<td><strong>PRATO PRINCIPAL:</strong> <br>', '')
+data1 = data1.replace('</td>','')
+print  data1
+noisnafita.close()
